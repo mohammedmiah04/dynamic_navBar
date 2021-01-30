16f2b55a542a891b920de0ab501ad5eea445fc61
MohammedM <nanumiah@nanus-mbp.home>
2021-01-30 12:20:21 +0000

    Updates

---
+++ precache-manifest.8a3a96fda41613a3234d6af23c6b9d1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c65bd66005bcd63f9a8681ad6353613",
+    "url": "/dynamic_navBar/index.html"
+  },
+  {
+    "revision": "0df96ef03c2b782bb4c6",
+    "url": "/dynamic_navBar/static/css/main.beaf029b.chunk.css"
+  },
+  {
+    "revision": "7e757b60e6d26cb3cfaa",
+    "url": "/dynamic_navBar/static/js/2.f60798fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dynamic_navBar/static/js/2.f60798fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df96ef03c2b782bb4c6",
+    "url": "/dynamic_navBar/static/js/main.61b297cd.chunk.js"
+  },
+  {
+    "revision": "d1fc8a3d3f58a7ceb6ba",
+    "url": "/dynamic_navBar/static/js/runtime-main.f223e2da.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/dynamic_navBar/static/media/logo.2bb7da65.svg"
+  }
+]);
